699f3663c3e3d9bc4eb7d92fe356abb0d73ff40f
Matt Lockyer <mattdlockyer@gmail.com>
2020-02-03 16:24:39 -0800

    update readme, remove smart contract and truffle files

---
+++ examples/node-contracts/config.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+let network, net, url, privateKey
+
+switch(process.env.ENV){
+    case 'local': {
+        network = 0;
+        net = 2;
+        url = process.env.LOCAL_0_URL
+        privateKey = process.env.LOCAL_PRIVATE_KEY
+        break;
+    }
+    case 'testnet': {
+        network = 1;
+        net = 2;
+        url = process.env.TESTNET_0_URL
+        break;
+    }
+    case 'mainnet': {
+        network = 2;
+        net = 1;
+        url = process.env.MAINNET_0_URL
+        break;
+    }
+}
+
+module.exports = {
+    port: 3000,
+    privateKey,
+    ENV: process.env.ENV,
+    network, // 0 local, 1 testnet, 2 mainnet
+    net, //TODO: change name
+    url,   
+}
